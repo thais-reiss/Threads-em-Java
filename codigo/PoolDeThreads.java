8c862f08fe8e28126d239e49d186e5da06c63d86
thais-reiss <thaisthatareis@gmail.com>
2026-08-09 19:44:02 -0300

    adiciona classe PoolDeThreads

---
+++ codigo/PoolDeThreads.java
@@ -0,0 +1,31 @@
+package codigo;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class PoolDeThreads {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService pool = Executors.newFixedThreadPool(4);
+        
+        long inicio = System.currentTimeMillis();
+
+        for (int i = 0; i < 10; i++) {
+            int idCliente = i;
+            pool.submit(() -> {
+                System.out.println(Thread.currentThread().getName());
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        pool.shutdown();
+        pool.awaitTermination(1, TimeUnit.MINUTES);
+
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo total: " + (fim - inicio) + " ms");
+    }
+}
